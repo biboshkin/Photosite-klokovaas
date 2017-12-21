6891556a11087ed267af358df5b5194fbd0f6557
Клоков Николай Анатольевич <SBT-Klokov-NA@sigma.sbrf.ru>
2017-12-21 19:37:51 +0300

    edit main menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,7 +9,7 @@ export default ({}) => (
 				<div className="logo">
 					<Link to="/">
 					 	<span className="menu-title">Клокова Анастасия</span>
-						<i className="menu-title__icon fa fa-camera fa-lg"></i>
+						<i className="menu-title__icon fa fa-camera fa-2x"></i>
 						<span className="menu-title">Фотограф</span>
 					</Link>
 					<h1 className="hide">Klokova Anastasia</h1> 
@@ -19,16 +19,19 @@ export default ({}) => (
 					<p><Link to="contact">Контакты</Link></p>
 				</div>
 			</div>
-			
 			<div className="social">
+				<i className="fa fa-phone fa-2x"><span className="menu-social__phone">+7 (910) 905 4647</span></i>
+				
+			</div>
+			<div className="social">	
 				<a href="https://vk.com/klokova_as">
-					<i className="fa fa fa-vk"></i>
+					<i className="fa fa-vk fa-2x"></i>
 				</a>
 				<a href="https://www.instagram.com/klokova_as/">
-					<i className="fa fa-instagram"></i>
+					<i className="fa fa-instagram fa-2x"></i>
 				</a>
 				<a href="mailto:klokovaas@mail.ru.com">
-					<i className="fa fa-envelope-o"></i>
+					<i className="fa fa-envelope-o fa-2x"></i>
 				</a>
 			</div>
 		</div>
